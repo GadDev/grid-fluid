e6247f1ce194c0830f68e680f96bfdf9b2059969
Alex <alex.gadaix@gmail.com>
2019-11-06 11:53:49 +0000

    Updates

---
+++ precache-manifest.95975347cd2b2313de50bd5a6ab8dccb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa6b360e09eb9d826e0cc721a464bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f03febe751828128557",
+    "url": "/static/css/main.2349d095.chunk.css"
+  },
+  {
+    "revision": "9afb965b535b3836daa4",
+    "url": "/static/js/2.6b993557.chunk.js"
+  },
+  {
+    "revision": "2f03febe751828128557",
+    "url": "/static/js/main.a744011d.chunk.js"
+  },
+  {
+    "revision": "33985db4ff9c1f920c7c",
+    "url": "/static/js/runtime-main.52a1a496.js"
+  }
+]);
